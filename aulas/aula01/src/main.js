a9d7ab9784ccfec43288edfa6d4bf90dd056a069
LightThe <gallotheo@icloud.com>
2026-03-19 21:50:03 -0300

    feat: adiciona validação no login da aula 01

--- aulas/aula01/src/main.js
+++ aulas/aula01/src/main.js
@@ -1,1 +1,35 @@
-import './style.css'
+import "./style.css";
+
+const botaoEntrar = document.querySelector("button");
+
+botaoEntrar.addEventListener("click", (e) => {
+  const inputMatricula = document.querySelector("#matricula");
+  const inputSenha = document.querySelector("#senha");
+
+  const matriculaErro = document.querySelector("#matricula-erro");
+  const senhaErro = document.querySelector("#senha-erro");
+  matriculaErro.textContent = '';
+  senhaErro.textContent = '';
+
+  if (inputMatricula.value === "") {
+    matriculaErro.textContent = "Matricula é obrigatório";
+    return;
+  }
+
+  if (parseInt(inputMatricula.value) <= 0) {
+    matriculaErro.textContent = "Matricula deve ser positivo";
+    return;
+  }
+
+  if (inputSenha.value === "") {
+    senhaErro.textContent = "Senha é obrigatório";
+    return;
+  }
+
+  if (inputSenha.value.length < 8) {
+    senhaErro.textContent = "Senha deve ser grande e revoltante";
+    return;
+  }
+
+  console.log("keep feeling, _validation_!");
+});
