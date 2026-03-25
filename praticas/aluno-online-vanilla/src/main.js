bbfb2ff8a121bed208b3103476eb6d6c9563aefb
LightThe <gallotheo@icloud.com>
2026-03-24 22:02:05 -0300

    feat: conclui pratica02. Fecha #3

--- praticas/aluno-online-vanilla/src/main.js
+++ praticas/aluno-online-vanilla/src/main.js
@@ -1,1 +1,30 @@
-import './style.css'
+import "./style.css";
+
+const formLogin = document.querySelector("form");
+
+formLogin.addEventListener("submit", (e) => {
+  const inputEmail = document.querySelector("#email");
+  const inputSenha = document.querySelector("#senha");
+  const emailErro = document.querySelector("#email-erro");
+  const senhaErro = document.querySelector("#senha-erro");
+
+  emailErro.textContent = "";
+  senhaErro.textContent = "";
+
+  var formValid = true;
+  if (inputEmail.value === "") {
+    emailErro.textContent = "O campo de e-mail é obrigatório";
+    formValid = false;
+  }
+
+  if (inputSenha.value === "") {
+    senhaErro.textContent = "O campo de senha é obrigatório";
+    formValid = false;
+  }
+
+  if (formValid) {
+    location.href = "/";
+  }
+
+  e.preventDefault();
+});
