b34eccbc07f160f177ad2db5a2cdf810cadacb90
LightThe <gallotheo@icloud.com>
2026-05-14 21:01:48 -0300

    feat: conclui pratica07. Fecha #13

--- praticas/aluno-online-react/src/components/SidebarLayout.jsx
+++ praticas/aluno-online-react/src/components/SidebarLayout.jsx
@@ -1,18 +1,26 @@
-import { Outlet } from "react-router";
+import { Navigate, Outlet } from "react-router";
 import Sidebar from "../components/Sidebar";
 import Topbar from "../components/Topbar";
+import useAuth from "../hooks/useAuth";
 
 export default function SidebarLayout() {
+  const { autenticado } = useAuth();
   return (
     <>
-      <Sidebar />
-      <main className="flex-1 m-4">
-        <Topbar />
-        <h2 className="text-2xl">Bem-vindo ao portal do aluno</h2>
-        <section>
-            <Outlet />
-        </section>
-      </main>
+      {autenticado ? (
+        <>
+          <Sidebar />
+          <main className="flex-1 m-4">
+            <Topbar />
+            <h2 className="text-2xl">Bem-vindo ao portal do aluno</h2>
+            <section>
+              <Outlet />
+            </section>
+          </main>
+        </>
+      ) : (
+        <Navigate to="/login" />
+      )}
     </>
   );
 }
